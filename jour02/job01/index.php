5c3efa3d47ab0a1d72a6fcd19029c45854534fde
camille-clappaz <camille.clappaz@laplateforme.io>
2023-01-10 11:01:54 +0100

    job01 OK attention pas de virgule entre if et else

---
+++ jour02/job01/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for($x=0; $x<=1337; $x++){
+        if($x==42){
+            echo '<b><u>' .$x.'</b></u>'.'</br>';
+        }
+        else{
+            echo $x .'</br>';
+        };
+    }
+
+    ?>
+</body>
+</html>
