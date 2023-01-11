7d0265a8117631ba975aa3605297f5d734881df4
camille-clappaz <camille.clappaz@laplateforme.io>
2023-01-11 10:42:00 +0100

    job01 OK, faire attention a la syntaxe, les simples et doubles cote

---
+++ jour03/job01/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$nombres=[200,204,173,98,171,404,459];
+for($i=0; $i<count($nombres); $i++){
+    if($nombres[$i]%2==0){
+        // echo $nombres[$i] .'est paire'. '</br>'; 
+        echo "$nombres[$i] est paire </br>"; 
+    }
+    else{
+        echo "$nombres[$i] est impaire </br>";  
+    }
+}
+    ?>
+</body>
+</html>
