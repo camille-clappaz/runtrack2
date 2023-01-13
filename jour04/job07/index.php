3eac807c94618e95c3263d70efcbf5ebfb864898
camille-clappaz <camille.clappaz@laplateforme.io>
2023-01-13 12:07:44 +0100

    Job7 bas maison OK mais le haut... ( à trouver )

--- jour04/job07/index.php
+++ jour04/job07/index.php
@@ -15,17 +15,29 @@
     <?php
 $hauteur= $_GET["hauteur"];
 $largeur= $_GET["largeur"];
-$i="*";
-for ($i = 0; $i < $hauteur; $i++){}
-// {echo $i};
-echo $i;
-// echo str_repeat('&nbsp;',($hauteur-$i));
-// for($j = 0; $j <= $i; $j++)
-// {
-// echo "* ";
-// }
-// echo "<br />";
-// }
-    ?>
+//triangle avec hauteur et largeur dynamique//
+for($i=0; $i<$hauteur;$i++){
+    // for($z=0; $z<$largeur; $z++){
+    //     echo "";
+    // }
+    for($k=0; $k<=$i; $k++){
+        echo "*";
+        
+    }
+    echo "</br>";
+}
+
+//Pour faire un rectangle avec hauteur et largeur dynamiques//
+for ($i = 0; $i < $hauteur; $i++)
+{
+for ($k = 0; $k < $largeur; $k++)
+{echo "*";}
+echo "<br />";
+}
+
+
+
+?>
+    
 </body>
 </html>
