e17b24298b8475b6531f4c5e8270e79705a68bee
camille-clappaz <camille.clappaz@laplateforme.io>
2023-01-17 14:05:47 +0100

    job 06ok! Attention aux = et ==!!!!!!!

--- jour07/job06/index.php
+++ jour07/job06/index.php
@@ -9,12 +9,34 @@
 <body>
     <?php
 function leetSpeak($str){
-    for($i=0; isset($str[$i])==true; $i++){
-    if($str[$i]== "A" || $str[$i]== "a"){
-        return $str[$i]=4;
-    }}
+    for($i=0; isset($str[$i]); $i++){   
+   if($str[$i]=="a" || $str[$i]=="A"){
+  $str[$i]=4;
+   }
+elseif ($str[$i]=="b" || $str[$i]=="B") {
+    $str[$i]=8;
 }
-$leet=leetSpeak("charade");
+elseif ($str[$i]=="E" || $str[$i]=="e") {
+    $str[$i]=3;
+}
+elseif ($str[$i]=="G" || $str[$i]=="g") {
+    $str[$i]=6;
+}
+elseif ($str[$i]=="L" || $str[$i]=="l") {
+    $str[$i]=1;
+}
+elseif ($str[$i]=="s" || $str[$i]=="S") {
+    $str[$i]=5;
+}
+elseif ($str[$i]=="T" || $str[$i]=="t") {
+    $str[$i]=7;
+}
+}
+    return $str;
+    }
+
+
+$leet=leetSpeak("Ballade dans les tiges");
 echo $leet;
     ?>
 </body>
