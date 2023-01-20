1e172c9216572fa2aa87fb18315c4a1434da7d26
camille-clappaz <camille.clappaz@laplateforme.io>
2023-01-20 11:13:00 +0100

    job 01 OK avec la synthaxe alternative

--- jour09/job16/job16.sql
+++ jour09/job16/job16.sql
@@ -1,1 +1,3 @@
 SELECT salles.nom, salles.capacite, etage.nom FROM salles INNER JOIN etage ON id_etage=etage.id ORDER BY capacite DESC LIMIT 1; 
+
+SELECT MAX(`salles`.`capacite`)as 'capacite', `salles`.`nom` as "Biggest Room", `etage`.`nom` FROM `salles` INNER JOIN `etage` ON `salles`.`id_etage` = `etage`.`id`;
